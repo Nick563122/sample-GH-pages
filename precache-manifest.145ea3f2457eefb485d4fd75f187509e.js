924b7d5456ed2a125903a43a5dfd70f2b24f1c7b
unknown <luceronicholas0@gmail.com>
2020-08-06 17:01:59 -0600

    Updates

---
+++ precache-manifest.145ea3f2457eefb485d4fd75f187509e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39bad2d7908650105a8c1aeeb932e901",
+    "url": "/Nick563122/sample-GH-pages/my-app/index.html"
+  },
+  {
+    "revision": "b65601b8067fdeeb6058",
+    "url": "/Nick563122/sample-GH-pages/my-app/static/css/main.3839607c.chunk.css"
+  },
+  {
+    "revision": "c743dcc7a7d635557717",
+    "url": "/Nick563122/sample-GH-pages/my-app/static/js/2.54ffdb41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Nick563122/sample-GH-pages/my-app/static/js/2.54ffdb41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b65601b8067fdeeb6058",
+    "url": "/Nick563122/sample-GH-pages/my-app/static/js/main.252d23e5.chunk.js"
+  },
+  {
+    "revision": "dabbb364e879c3f38835",
+    "url": "/Nick563122/sample-GH-pages/my-app/static/js/runtime-main.d3ab89d7.js"
+  }
+]);
