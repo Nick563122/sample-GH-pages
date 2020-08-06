21bebc0ea70b7bdc51162aca80388b53aa3711cd
unknown <luceronicholas0@gmail.com>
2020-08-06 17:15:45 -0600

    Updates

---
+++ precache-manifest.eecde993f0f73b2287b8919ab752c8a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ba82fd97917f27fdeb56c3c1cf779c",
+    "url": "/Nick563122/sample-GH-page/my-app/index.html"
+  },
+  {
+    "revision": "84c763930cbdf180cebe",
+    "url": "/Nick563122/sample-GH-page/my-app/static/css/main.3839607c.chunk.css"
+  },
+  {
+    "revision": "c743dcc7a7d635557717",
+    "url": "/Nick563122/sample-GH-page/my-app/static/js/2.54ffdb41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Nick563122/sample-GH-page/my-app/static/js/2.54ffdb41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84c763930cbdf180cebe",
+    "url": "/Nick563122/sample-GH-page/my-app/static/js/main.811b3d94.chunk.js"
+  },
+  {
+    "revision": "ffc0c10ccea5789eaf3b",
+    "url": "/Nick563122/sample-GH-page/my-app/static/js/runtime-main.a4420dff.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Nick563122/sample-GH-pages/my-app/precache-manifest.145ea3f2457eefb485d4fd75f187509e.js"
+  "/Nick563122/sample-GH-page/my-app/precache-manifest.eecde993f0f73b2287b8919ab752c8a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Nick563122/sample-GH-pages/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Nick563122/sample-GH-page/my-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
